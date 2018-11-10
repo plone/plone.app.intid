80c92ee00f987b6bbbd25d02c01d7f309c659260
Gil Forcada Codinachs <gil.forcada@freitag.de>
2018-11-10 21:54:39 +0100

    Update trove classifiers
    [ci-skip]

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords='plone zope five intid',
     author='Alec Mitchell',
